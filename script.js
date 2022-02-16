028293c871826daee8c34e43f82fd6dc76376feb
ErenEmek <52907887+ErenEmek@users.noreply.github.com>
2022-02-16 21:12:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function validate() {
+  if (document.getElementById("principal").value <= 0) {
+    alert("please enter a positive value as principal");
+    document.getElementById("principal").focus();
+  }
 }
-        
+
+function compute() {
+
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * years * rate / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest + ",\<br\>in the year " + year + "\<br\>";
+
+}
+
+var rateval = document.getElementById("rate");
+document.getElementById("rate_val").innerHTML = rateval.value;
+
+rateval.oninput = function() {
+  document.getElementById("rate_val").innerHTML = this.value;
+}
+
